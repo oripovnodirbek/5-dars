6fc3d753b8857876dd4f9a809b1dd177f991795f
oripovnodirbek <nodirbekoripov1333@gmail.com>
2024-04-25 01:39:41 +0500

    for

---
+++ app.js
@@ -0,0 +1,80 @@
+// const n = prompt('birinchi sonni kiriting')
+// const k = prompt('ikkinchi sonni kiriting')
+// for (let i = 0; i < n  ; i++){
+//     console.log(k)
+// }
+
+
+
+// let num1 = +prompt('birinchi sonni kiriting')
+// let num2 = +prompt('ikkinchi sonni kiriting')
+// for(let i = num1; i <= num2; i++) {
+//       console.log(i)
+// }
+
+
+// let num1 = +prompt('birinchi sonni kiriting')
+// let num2 = +prompt('ikkinchi sonni kiriting')
+// let counter = 0
+// for(let i = num2; i > num1; i--) {
+//     let num2 = "num2 - 1"
+//       console.log(i)
+//       counter++
+// }
+// console.log(counter + ' ta son bor')
+
+
+// let narx = +prompt('Narxni belgilang')
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${i*narx} so'm`)
+// }
+
+
+// bunda ishlayabdi lkn ozgina muammo bor domladan sorab olaman.
+// let narx = +prompt('Narxni belgilang')
+// for(let i = 0.1; i <= 1; i += 0.1){
+//     console.log(`${i*narx} so'm`)
+// }
+
+
+// let num1 = +prompt("Boshlang'ich sonni kiriting")
+// let num2 = +prompt("oxirgi sonni kiriting")
+// let sum = 0
+// for(let i = num1; i <=num2; i++){
+//     sum += i
+// }
+// console.log("yig'indisi: " + sum)
+
+
+// let num1 = +prompt("Boshlang'ich sonni kiriting")
+// let num2 = +prompt("oxirgi sonni kiriting")
+// let sum = 1
+// for(let i = num1; i <=num2; i++){
+//     sum *= i
+// }
+// console.log("Ko'paytmasi: " + sum)
+
+
+// let num1 = +prompt("Boshlang'ich sonni kiriting")
+// let num2 = +prompt("oxirgi sonni kiriting")
+// let sum = 1
+// for(let i = num1; i <=num2; i++){
+//     sum *= i**2
+// }
+// console.log("Darajasi: " + sum)
+
+
+// let n = +prompt('Sonni kiriting')
+// let sum = 0
+// for(let i =  0;  i<=n ; i++ ){
+//     sum = i**2 +(i+2)**2 + (i*2)**2
+// }
+// console.log(sum)
+
+
+// let n = +prompt('sonni kiriting')
+// let sum = 1
+// for(let i = 1; i <= n; i++){
+//     sum *= (1+i*0.1)
+// }
+// console.log('kopaytmasi: ' + sum)
